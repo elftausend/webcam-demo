f1d91cbdfd0d37739a7ccf83619d4225954ad060
elftausend <76885970+elftausend@users.noreply.github.com>
2023-07-06 20:36:12 +0200

    Working texture uniform

--- src/main2.rs
+++ src/main2.rs
@@ -1,3 +1,5 @@
+use std::mem::size_of;
+
 use glow::*;
 
 
@@ -20,15 +22,19 @@ pub fn main2() {
         };
 
 
-        let width = 1920;
-        let height = 1080;
+        //gl.enable(glow::BLEND);
+        gl.enable(DEBUG_OUTPUT);
 
-        let (buf, vertex_array) = create_vertex_buffer(&gl);
+
+        let width = 300;
+        let height = 300;
+
 
         let program = gl.create_program().expect("Cannot create program");
 
         let (vertex_shader_source, fragment_shader_source) = (
-            r#"in vec2 position;
+            r#"
+            in vec2 position;
             in vec2 tex_coords;
             out vec2 v_tex_coords;
             
@@ -37,9 +43,9 @@ pub fn main2() {
                 v_tex_coords = tex_coords;
             }"#,
             r#"
-            uniform sampler2D tex;
             in vec2 v_tex_coords;
             out vec4 f_color;
+            uniform sampler2D tex;
             void main() {
                 f_color = texture(tex, v_tex_coords);
             }
@@ -76,43 +82,65 @@ pub fn main2() {
             gl.delete_shader(shader);
         }
 
-
         let texture = gl.create_texture().expect("Cannot create texture");
         gl.active_texture(TEXTURE0);
-
-        
         gl.bind_texture(glow::TEXTURE_2D, Some(texture));
-        gl.tex_storage_2d(texture.0.into(), 1, glow::RGBA8, width, height);
-
-        // upload data to texture
-        let data = vec![255u8; width as usize * height as usize * 4];
-        gl.tex_sub_image_2d(
-            texture.0.into(),
+        //gl.tex_storage_2d(glow::TEXTURE_2D, 1, glow::RGBA8, width, height);
+        let data = vec![120u8; width as usize * height as usize * 4];
+        gl.tex_image_2d(
+            glow::TEXTURE_2D,
             0,
-            0,
-            0,
+            glow::RGBA as i32,
             width,
             height,
+            0,
             glow::RGBA,
             glow::UNSIGNED_BYTE,
-            PixelUnpackData::Slice(&data)            
+            Some(&data) 
         );
 
-        let uniform_location = gl.get_uniform_location(program, "tex").unwrap();
-        // set 'texture' to sampler2D tex in fragment shader
-        gl.uniform_1_i32(Some(uniform_location).as_ref(), 0);
-    
-
+        // necessary for drawing!
+        gl.generate_mipmap(glow::TEXTURE_2D);
+//
         gl.use_program(Some(program));
-        gl.clear_color(0.1, 0.2, 0.3, 1.0);
 
+
+        let (buf, vertex_array, ebo) = create_vertex_buffer(&gl);
+
+        // set 'texture' to sampler2D tex in fragment shader
+        
+        gl.uniform_1_i32(gl.get_uniform_location(program, "tex").as_ref(), 0);
+
+        gl.bind_texture(glow::TEXTURE_2D, None);
+
+    
         // We handle events differently between targets
         {
             use glutin::event::{Event, WindowEvent};
             use glutin::event_loop::ControlFlow;
 
             event_loop.run(move |event, _, control_flow| {
-                *control_flow = ControlFlow::Wait;
+                //*control_flow = ControlFlow::Wait;
+
+                gl.clear_color(0.1, 0.2, 0.3, 0.3);
+                
+                //gl.enable(glow::TEXTURE_2D);
+
+                gl.clear(glow::COLOR_BUFFER_BIT);
+
+                gl.bind_texture(glow::TEXTURE_2D, Some(texture));
+                gl.bind_vertex_array(Some(vertex_array));
+                gl.use_program(Some(program));
+                gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
+                let uniform_location = gl.get_uniform_location(program, "tex");
+                gl.uniform_1_i32(uniform_location.as_ref(), 0);
+
+                gl.draw_arrays(glow::TRIANGLE_STRIP, 0, 5);
+                //gl.draw_elements(glow::TRIANGLES, 6, glow::UNSIGNED_INT, 0);
+                
+                window.swap_buffers().unwrap();            
+                gl.use_program(None);
+
                 match event {
                     Event::LoopDestroyed => {
                         return;
@@ -121,12 +149,7 @@ pub fn main2() {
                         window.window().request_redraw();
                     }
                     Event::RedrawRequested(_) => {
-                        gl.clear(glow::COLOR_BUFFER_BIT);
-
-                        gl.bind_texture(glow::TEXTURE_2D, Some(texture));
                         
-                        gl.draw_arrays(glow::TRIANGLES, 0, 4);
-                        window.swap_buffers().unwrap();
                     }
                     Event::WindowEvent { ref event, .. } => match event {
                         WindowEvent::Resized(physical_size) => {
@@ -148,29 +171,94 @@ pub fn main2() {
 
 
 
-unsafe fn create_vertex_buffer(gl: &glow::Context) -> (NativeBuffer, NativeVertexArray) {
+unsafe fn create_vertex_buffer(gl: &glow::Context) -> (NativeBuffer, NativeVertexArray, NativeBuffer) {
+    let indices = [0u32, 2, 1, 0, 3, 2];
+
     // This is a flat array of f32s that are to be interpreted as vec2s.
-    let triangle_vertices = [-1., -1., 0., 0., -1., 1., 0., 1., 1., 1., 1., 1., 1., -1., 1., 0.];
+    #[rustfmt::skip]
+    let triangle_vertices = [
+        -1f32, -1., 
+        -1., 1., 
+        1., 1., 
+        1., -1.
+    ];
+
+    #[rustfmt::skip]
+    let texcoords = [
+        0f32, 0., 
+        0., 1., 
+        1., 1., 
+        1., 0.
+    ];
+
+
+    #[rustfmt::skip]
+    let triangle_vertices = [
+        -0.5f32, -0.5,
+        0.5, -0.5,
+        -0.5, 0.5,
+        0.5, 0.5,
+    ];
+
+    #[rustfmt::skip]
+    let texcoords = [
+        0f32, 0.,
+        1., 0.,
+        0., 1.,
+        1., 1.,
+    ];
 
     let triangle_vertices_u8: &[u8] = core::slice::from_raw_parts(
         triangle_vertices.as_ptr() as *const u8,
         triangle_vertices.len() * core::mem::size_of::<f32>(),
     );
 
-    // We construct a buffer and upload the data
-    let vbo = gl.create_buffer().unwrap();
-    gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
-    gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, triangle_vertices_u8, glow::STATIC_DRAW);
+    let tex_coords_u8 = core::slice::from_raw_parts(
+        texcoords.as_ptr() as *const u8,
+        texcoords.len() * core::mem::size_of::<f32>(),
+    );
 
-    // We now construct a vertex array to describe the format of the input buffer
+    let indices_u8 = core::slice::from_raw_parts(
+        indices.as_ptr() as *const u8,
+        indices.len() * core::mem::size_of::<u32>(),
+    );
+
+    let ebo = gl.create_buffer().unwrap();
+    gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
+    gl.buffer_data_u8_slice(glow::ELEMENT_ARRAY_BUFFER, indices_u8, glow::STATIC_DRAW);
+
     let vao = gl.create_vertex_array().unwrap();
     gl.bind_vertex_array(Some(vao));
-    gl.enable_vertex_attrib_array(0);
-    
-    
-    //gl.vertex_attrib_pointer_f32(0, 2, glow::FLOAT, false, 8, 0);
-    // look in docs
-    gl.vertex_attrib_pointer_f32(0, 4, glow::FLOAT, false, 16, 0);
 
-    (vbo, vao)
+    let vbo = gl.create_buffer().unwrap();
+    gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
+    gl.buffer_data_size(glow::ARRAY_BUFFER, (16 * size_of::<f32>()) as i32, glow::STATIC_DRAW);
+    gl.buffer_sub_data_u8_slice(glow::ARRAY_BUFFER, 0, triangle_vertices_u8);
+    gl.buffer_sub_data_u8_slice(glow::ARRAY_BUFFER, (8 * size_of::<f32>()) as i32, tex_coords_u8);
+    
+
+    // We construct a buffer and upload the data
+    //let vbo = gl.create_buffer().unwrap();
+    //gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
+    //gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, triangle_vertices_u8, glow::STATIC_DRAW);
+
+    // We now construct a vertex array to describe the format of the input buffer
+    //let vao = gl.create_vertex_array().unwrap();
+    //gl.bind_vertex_array(Some(vao));
+    
+    // gl.vertex_attrib_pointer_f32(0, 2, glow::FLOAT, false, 8, 0);
+    gl.vertex_attrib_pointer_f32(0, 2, glow::FLOAT, false, 8, 0);
+    gl.enable_vertex_attrib_array(0);
+    // gl.vertex_attrib_pointer_f32(1, 2, glow::FLOAT, false, 8, 0);
+    //gl.vertex_attrib_pointer_i32(index, size, data_type, stride, offset)
+    gl.vertex_attrib_pointer_f32(1, 2, glow::FLOAT, false, 8, 8 * size_of::<f32>() as i32);
+    gl.enable_vertex_attrib_array(1);
+    
+
+    gl.bind_buffer(glow::ARRAY_BUFFER, None);
+    gl.bind_vertex_array(None);
+    gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, None);
+
+
+    (vbo, vao, ebo)
 }
